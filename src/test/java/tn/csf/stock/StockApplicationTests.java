d8cb6bc0e216d27f1b21779e3d1d5e228c5a73b2
nadiajlassifrigui <nadia.jlassi@gmail.com>
2023-08-14 10:18:27 +0100

    first commit

---
+++ src/test/java/tn/csf/stock/StockApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.csf.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
